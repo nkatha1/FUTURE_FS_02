666c618ea2f81343a39ab3bea9129a223192bda6
nkatha <125828300+nkatha1@users.noreply.github.com>
2025-09-08 19:53:33 +0300

    Add products route to retrieve all products

---
+++ backend/routes/products.js
@@ -0,0 +1,10 @@
+const express = require("express");
+const router = express.Router();
+const products = require("../data/products.json");
+
+// GET all products
+router.get("/", (req, res) => {
+  res.json(products);
+});
+
+module.exports = router;
